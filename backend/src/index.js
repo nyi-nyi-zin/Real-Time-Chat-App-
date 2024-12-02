4431926e3c9394b68638243ebe3284d6b58550d7
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2024-12-03 04:44:43 +0630

    database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 //import packages
 import express from "express";
+import dotenv from "dotenv";
 
+//Import local files
+import { connectDB } from "./lib/db.js";
+
+//Register packages
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
 
 //import routes
 import authRoutes from "./routes/auth.route.js";
@@ -9,4 +16,7 @@ import authRoutes from "./routes/auth.route.js";
 //Register routes
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, console.log("server is running on port 5001"));
+app.listen(PORT, () => {
+  console.log("server is running on port " + PORT);
+  connectDB();
+});
